a5bf5929cc63470c713e06508166fd03f2a2dba7
mnagari <mnagari@mun.ca>
2021-05-20 23:33:04 +0530

    added pcost sol Ex 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+f = open('Data/portfolio.csv','rt')
+tc = 0
+hdrs = next(f)
+for line in f:
+    r = line.split(',')
+    tc += int(r[1])*float(r[2])
+print("Total cost",tc)
