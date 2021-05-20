1df166d619d402d0ed0d3031bc085700ef7450e1
mnagari <mnagari@mun.ca>
2021-05-20 23:54:32 +0530

    added function to pcost - Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,10 +3,14 @@
 # Exercise 1.27
 
 
-f = open('Data/portfolio.csv','rt')
-tc = 0
-hdrs = next(f)
-for line in f:
-    r = line.split(',')
-    tc += int(r[1])*float(r[2])
-print("Total cost",tc)
+def portfolio_cost(filename):
+    ...
+    # Your code here
+    ...
+    f = open(filename,'rt')
+    tc = 0
+    hdrs = next(f)
+    for line in f:
+        r = line.split(',')
+        tc += int(r[1])*float(r[2])
+    print("Total cost",tc)
