abfcf47fca9ab871d5549fa94034a438228883a8
mnagari <mnagari@mun.ca>
2021-05-21 00:06:51 +0530

    added error handling - Exercise 1.31 completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,10 +7,15 @@ def portfolio_cost(filename):
     ...
     # Your code here
     ...
+    
     f = open(filename,'rt')
     tc = 0
     hdrs = next(f)
     for line in f:
         r = line.split(',')
-        tc += int(r[1])*float(r[2])
+        try:
+            tc += int(r[1])*float(r[2])
+        except:
+            print("Missing field value, please check")
+            return
     print("Total cost",tc)
